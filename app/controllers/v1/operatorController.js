371e5005d8aa2a424a31077cbce442aaebaa775d
Andy Williams-Jones <hello@awj.digital>
2019-06-09 17:51:56 +0100

    Search results

--- app/controllers/v1/operatorController.js
+++ app/controllers/v1/operatorController.js
@@ -1,17 +1,42 @@
 const version = "v1"
+const registerData = require('../../data/register.json')
 
 exports.operator_summary_get = function (req, res) {
 
-    console.log(version);
-        res.render(version + '/operator/summary', {
-            version
-        })
+    res.render(version + '/operator/summary', {
+        version
+    })
 }
 
 exports.operator_search_get = function (req, res) {
 
-    console.log(version);
+    var r = Math.random();
+    console.log(r)
+
+    if (r > 0.5) {
         res.render(version + '/operator/search', {
-            version
+            version,
+            registerData
         })
+    } else {
+        res.render(version + '/operator/search-b', {
+            version,
+            registerData
+        })
+    }
+}
+
+exports.operator_results_get = function (req, res) {
+
+    // Value from the form
+    var query = req.body.search;
+    var count = 0;
+
+    // Query the file
+
+    res.render(version + '/operator/results', {
+        version,
+        count,
+        registerData
+    })
 }
--- app/routes.js
+++ app/routes.js
@@ -17,5 +17,7 @@ router.get('/' + version + '/hub/', hub_Controller.home_get);
 // Operator
 router.get('/' + version + '/operator/summary', operator_Controller.operator_summary_get);
 router.get('/' + version + '/operator/search', operator_Controller.operator_search_get);
+router.post('/' + version + '/operator/results', operator_Controller.operator_results_get);
+router.get('/' + version + '/operator/results', operator_Controller.operator_results_get);
 
 module.exports = router
